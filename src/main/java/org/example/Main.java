4dc085619a2ac5f81099e6f82525a2e0ee40a08a
RungDaRongDa <jbj2720@gmail.com>
2023-05-17 11:47:12 +0900

    중간에 사진을 넣는 코드를 발견하여 이걸로 바꿨다. 뒤에 휜배경 추가 필요

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,131 +1,116 @@
 package org.example;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.client.j2se.MatrixToImageConfig;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-
-import javax.imageio.ImageIO;
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.*;
-import java.util.Base64;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Main main = new Main();
-        main.createQR();
-    }
+        // 저장될 파일 위치
+        private final String DIR = "c:\\test\\";
+        // 파일 확장자
+        private final String ext = ".png";
+        // QR CODE에 덮혀질 이미지
+        private final String LOGO = "C:\\Users\\JangBeomJun\\Downloads\\Group 1 (2).jpg";
+        // QR CODE 내용 or URL
+        private final String CONTENT = "2313 jang";
+        // 가로
+        private final int WIDTH = 300;
+        // 세로
+        private final int HEIGHT = 300;
 
-    String createQR() {
+        public void generate() {
 
-        BitMatrix bitMatrix = null;
-        // QRCode에 담고 싶은 정보를 문자열로 표시한다. url이든 뭐든 가능하다.
-        String codeInformation = "wwww";
+            Map<EncodeHintType, ErrorCorrectionLevel> hints = new HashMap<>();
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
 
-        // 큐알코드 바코드 및 배경 색상값
-        int onColor = 0xFF000000; // 바코드 색
-        int offColor = 0xFFFFFFFF; // 배경 색
+            QRCodeWriter writer = new QRCodeWriter();
+            BitMatrix bitMatrix = null;
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
 
-        // 이름 그대로 QRCode 만들때 쓰는 클래스다
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        // 큐알 전경과 배경의 색을 정한다. 값을 넣지 않으면 검정코드에 흰 배경이 기본값이다.
-        MatrixToImageConfig matrixToImageConfig = new MatrixToImageConfig(onColor, offColor);
-        Map<EncodeHintType, String> hints  = new HashMap<>();
-        // QRCode 생성시 조건을 넣어서 만들 수 있게 한다.
-        // 여기서 Error_Correction의 경우 큐알 코드가 좀더 자글자글하게 만들어 주는 대신 큐알이 가려져도 인식할 가능성이 더욱 높아진다.
+            try {
+                initDirectory(DIR);
 
-       /*
- 	https://zxing.github.io/zxing/apidocs/com/google/zxing/qrcode/decoder/ErrorCorrectionLevel.html
-        Enum Constants
-        L = ~7% correction
-        M = ~15% correction
-        Q = ~25% correction
-        H = ~30% correction
-        */
-        hints.put(EncodeHintType.ERROR_CORRECTION, "H");
+                bitMatrix = writer.encode(CONTENT, BarcodeFormat.QR_CODE, WIDTH, HEIGHT, hints);
+                BufferedImage qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix, getMatrixConfig());
+                BufferedImage overly = getOverly(LOGO);
+
+                int deltaHeight = qrImage.getHeight() - overly.getHeight();
+                int deltaWidth = qrImage.getWidth() - overly.getWidth();
+
+                BufferedImage combined = new BufferedImage(qrImage.getHeight(), qrImage.getWidth(), BufferedImage.TYPE_INT_ARGB);
+                Graphics2D g = (Graphics2D) combined.getGraphics();
+
+                g.drawImage(qrImage, 0, 0, null);
+                g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+                // QR코드 이미지의 정중앙 위치에 덮음
+                g.drawImage(overly, (int) Math.round(deltaWidth / 2), (int) Math.round(deltaHeight / 2), null);
 
 
-        // QRCode 전체 크기
-        // 단위는 fixel
-        int width = 1000;
-        int height = 1000;
+                ImageIO.write(combined, "png", os);
 
-        // 내부에 빈 공간만들 빈 공간 -> oncolor로 만들어진다.
-        int regionWidth=300;
-        int regionHeight=100;
+                Files.copy( new ByteArrayInputStream(os.toByteArray()), Paths.get(DIR + "stone" +ext), StandardCopyOption.REPLACE_EXISTING);
 
-        try {
-            // bitMatrix 형식으로 QRCode를 만든다.
-            bitMatrix = qrCodeWriter.encode(codeInformation, BarcodeFormat.QR_CODE, width, height);
-            // QRCode 중간에 빈공간을 만들고 색을 offColor로 바꿔주는 메소드
-             bitMatrix= emptyQR(bitMatrix,regionHeight,regionWidth); // QR내부에 빈 공간 만드는 메소드(사용할 경우 hint의 error_correction 을 반드시 높여줘야 합니다)
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream, matrixToImageConfig);
-
-            // 이제 만들어본 QRCode를 저장해보자
-            String savePath = "c:\\test\\" ;
-            String saveFileName = "qrImage.png";
-            File file = new File(savePath);
-            if (!file.exists()) {
-                file.mkdirs();
-                // 리눅스 서버에 저장하는 경우 파일 접근 권한을 줘야 한다.
-            }
-            // 파일은 저장하고 싶은대로 저장하면 된다.
-            // buffer나 stream는 공부를 더 해봐야 하는 부분이다.
-
-            // bufferedImage 를 이용한 파일 저장 -> 방식 1
-            BufferedImage bufferedImage = null;
-            bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix, matrixToImageConfig);
-            File saveFile = new File(savePath + "test");
-            ImageIO.write(bufferedImage, "png", saveFile);
-
-            // fileOutputStream 을 이용한 파일 저장 -> 방식 2
-            FileOutputStream fileOutputStream = new FileOutputStream(new File(savePath + "test"));
-            fileOutputStream.write(outputStream.toByteArray());
-            fileOutputStream.close();
-
-            // byteArray를 base64로 변환한 이유는 프론트에서 파일경로가 아닌 binary 형식으로 전송해서 보여주기 위해서다.
-            // 이렇게 할 경우 DB에 이미지를 저장하지 않고 화면에 보여줄 수 있다.
-            return Base64.getEncoder()
-                    .encodeToString(outputStream.toByteArray());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private BitMatrix emptyQR(BitMatrix bitMatrix, int regionHeight, int regionWidth) {
-        // 이 메소드는 bitmatrix에 네모난 공간을 만드는 것이다.
-
-        // 빈 공간의 넓이와 높이
-        int width=bitMatrix.getWidth();
-        int height=bitMatrix.getHeight();
-
-        // 빈 공간의 위치(중앙으로 설정했다.)
-        int left=(width-regionWidth)/2;
-        int top=(height-regionHeight)/2;
-
-        // 빈 공간 생성하기(이때 색은 offColor)
-        bitMatrix.setRegion(left,top,regionWidth,regionHeight);
-        // 빈 공간의 색을 배경색으로 반전시킨다.(fixel 단위로 찾아서 색을 뒤집는다.)
-        for (int y = top; y <= top+regionHeight; y++) {
-            for (int x = left; x <= left+regionWidth; x++) {
-                if(bitMatrix.get(x, y)){
-                    bitMatrix.unset(x,y);
-                };
+            } catch (WriterException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        return bitMatrix;
-    }
+
+        private BufferedImage getOverly(String LOGO) throws IOException {
+            File url = new File(LOGO);
+            return ImageIO.read(url);
+        }
+
+        private void initDirectory(String DIR) throws IOException {
+            Files.createDirectories(Paths.get(DIR));
+        }
+
+        private MatrixToImageConfig getMatrixConfig() {
+            return new MatrixToImageConfig(Main.Colors.BLACK.getArgb(), Main.Colors.WHITE.getArgb());
+        }
+
+        public enum Colors {
+            BLUE(0xFF40BAD0),
+            RED(0xFFE91C43),
+            PURPLE(0xFF8A4F9E),
+            ORANGE(0xFFF4B13D),
+            WHITE(0xFFFFFFFF),
+            BLACK(0xFF000000);
+            private final int argb;
+            Colors(final int argb){
+                this.argb = argb;
+            }
+            public int getArgb(){
+                return argb;
+            }
+        }
+
+        public static void main(String[] args) throws WriterException, IOException {
+            Main main = new Main();
+            main.generate();
+        }
+
 }
 
 
